aa8d9655a5add16d4ca275fadaf7fa9fe5012def
walker <stopa3.10.1996@mail.ru>
2020-04-05 00:31:54 +0300

    Add html minimizer

--- minimize.py
+++ minimize.py
@@ -1,6 +1,16 @@
-from csscompressor import compress
+from csscompressor import compress as compress_css
+from htmlmin import minify as compress_html
 from os import walk
 from os.path import join as join_path
+from typing import Callable
+
+
+def process_file(path: str, proc: Callable[[str], str]):
+    with open(path, "r") as f:
+        out = proc(f.read())
+    with open(path, "w") as f:
+        f.write(out)
+    print("Process " + path)
 
 
 if __name__ == '__main__':
@@ -8,8 +18,6 @@ if __name__ == '__main__':
         for file in files:
             path = join_path(root, file)
             if path.endswith(".css"):
-                with open(path, "r") as f:
-                    out = compress(f.read())
-                with open(path, "w") as f:
-                    f.write(out)
-                print("Process " + path)
+                process_file(path, compress_css)
+            elif path.endswith(".html"):
+                process_file(path, compress_html)
