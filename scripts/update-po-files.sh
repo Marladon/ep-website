a9e84b0277eae7aefe57484e17970ceae8841749
mihalin <stopa3.10.1996@mail.ru>
2020-05-04 15:12:30 +0300

    Add margin to intro image

--- scripts/update-po-files.sh
+++ scripts/update-po-files.sh
@@ -1,2 +1,2 @@
-pygettext3.7 -d en -o locale/en_US/LC_MESSAGES/en.pot data/*.py
+pygettext3 -d en -o locale/en_US/LC_MESSAGES/en.pot data/*.py
 msgmerge locale/en_US/LC_MESSAGES/en.po locale/en_US/LC_MESSAGES/en.pot --update
